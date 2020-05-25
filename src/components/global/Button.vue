f82631f27d8c07f81de0d1e3df946075a6b0d166
Rafael Zago <rafaelszago@gmail.com>
2020-05-25 17:02:06 -0300

    Start component example

---
+++ src/components/global/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="btn">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'base-button'
+}
+</script>
+
+<style lang="scss">
+.btn {
+  background-color: #1273e3;
+  color: #ffffff;
+}
+</style>
