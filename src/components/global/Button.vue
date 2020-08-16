9ac91c41942586bfc80c6c4743bec6539547b9c3
Rafael Zago <rafaelszago@gmail.com>
2020-08-16 13:12:57 -0300

    Updating button styles

--- src/components/global/Button.vue
+++ src/components/global/Button.vue
@@ -13,6 +13,16 @@ export default {
 <style lang="scss">
 .btn {
   background-color: #1273e3;
+  border-radius: 2rem;
+  padding: 0.6rem 1.4rem;
   color: #ffffff;
+  border: 0;
+  outline: 0;
+  cursor: pointer;
+  transition: background 200ms ease;
+
+  &:hover {
+    background-color: #0d61c2;
+  }
 }
 </style>
