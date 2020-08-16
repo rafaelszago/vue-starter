832bf1be4b1070f3576224d1c22d2289c3e1553c
Rafael Zago <rafaelszago@gmail.com>
2020-08-16 13:09:31 -0300

    Import navbar component

--- src/layouts/Pages.vue
+++ src/layouts/Pages.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
+    <navbar />
     <h1>Pages</h1>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: 'pages-layout',
+
+  components: {
+    navbar: () => import('../components/Navbar')
+  }
+}
+</script>
