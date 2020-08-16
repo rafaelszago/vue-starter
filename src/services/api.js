d8dfff0302b64170021cfda114485d3deabc66d5
Rafael Zago <rafaelszago@gmail.com>
2020-08-16 13:26:53 -0300

    Updating services

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,10 @@
 import apisauce from 'apisauce'
-import { StorageService } from '../services'
-import store from '../store'
 
-const api = apisauce.create({ baseURL: process.env.VUE_APP_API_URL })
+const api = apisauce.create({
+  baseURL: process.env.VUE_APP_API_URL
+})
 
 const ApiService = {
-  _errorHandler: null,
-
-  setHeader () {
-    api.setHeader('Authorization', `Bearer ${StorageService.getToken()}`)
-  },
-
-  removeHeader () {
-    api.setHeader('Authorization', false)
-  },
-
   index (resource, params) {
     return api.get(resource, params)
   },
@@ -38,7 +28,7 @@ const ApiService = {
   addErrorMonitor () {
     return api.addMonitor(async response => {
       if (response.status === 401) {
-        await store.dispatch('auth/logout')
+        // do something
       }
     })
   }
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,1 @@
 export { default as ApiService } from './api'
-export { default as StorageService } from './storage'
-export { default as UserService } from './user'
